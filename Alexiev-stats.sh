fbfa2ef2ad9decc7d249dc79dee85415ae007389
Vladimir Alexiev <vladimir.alexiev@ontotext.com>
2020-02-28 15:49:44 +0200

    update bib count, resume

--- Alexiev-stats.sh
+++ Alexiev-stats.sh
@@ -3,8 +3,8 @@
 
 # ./Alexiev-stats.sh > ./Alexiev-stats.txt
 
-echo "Book (author), thesis, conference proceedings (editor)	" `grep -cEi '@(.*thesis|book|proceedings){' *.bib`
-echo "Tech reports, manuals, deliverables, ontologies	" `grep -cEi '@(manual|techreport){'         *.bib` 
-echo "Conference papers, journal articles	" `grep -cEi '@(inproceedings|article){'     *.bib`
-echo "Presentations, posters, blog posts	" `grep -cEi '{(presentation|poster|blog)}'  *.bib`
-echo "TOTAL	" `grep -cEi '@[a-z]+{'                      *.bib`
+echo "Book (author), thesis, conference proceedings (editor)	" `grep -cEi '@(.*thesis|book|proceedings){'      *.bib`
+echo "Tech reports, manuals, deliverables, ontologies	" `grep -cEi '@(manual|techreport){'              *.bib` 
+echo "Conference papers, journal articles	" `grep -cEi '@(inproceedings|article){'          *.bib`
+echo "Presentations, posters, blog posts	" `grep -cEi '{(presentation|poster|blog|gist)}'  *.bib`
+echo "TOTAL	" `grep -cEi '@[a-z]+{'                           *.bib`
