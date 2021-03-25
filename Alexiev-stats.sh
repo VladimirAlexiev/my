e2146dc43f5a0d4908796133b307401637f0bbc9
Vladimir Alexiev <vladimir.alexiev@ontotext.com>
2021-03-25 11:30:05 +0200

    add GS1 EPCIS & CBV 2, update Alexiev-stats.txt

--- Alexiev-stats.sh
+++ Alexiev-stats.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # -*- tab-width: 62; -*-
 
-# ./Alexiev-stats.sh > ./Alexiev-stats.txt
+# ./Alexiev-stats.sh
 
-echo "Book (author), thesis, conference proceedings (editor)	" `grep -cEi '@(.*thesis|book|proceedings){'      *.bib`
-echo "Tech reports, manuals, deliverables, ontologies	" `grep -cEi '@(manual|techreport){'              *.bib` 
-echo "Conference papers, journal articles	" `grep -cEi '@(inproceedings|article){'          *.bib`
-echo "Presentations, posters, blog posts	" `grep -cEi '{(presentation|poster|blog|gist)}'  *.bib`
-echo "TOTAL	" `grep -cEi '@[a-z]+{'                           *.bib`
+echo "Book (author), thesis, conference proceedings (editor)	" `grep -cEi '@(.*thesis|book|proceedings){'      *.bib`  > ./Alexiev-stats.txt
+echo "Tech reports, manuals, deliverables, ontologies	" `grep -cEi '@(manual|techreport){'              *.bib` >> ./Alexiev-stats.txt
+echo "Conference papers, journal articles	" `grep -cEi '@(inproceedings|article){'          *.bib` >> ./Alexiev-stats.txt
+echo "Presentations, posters, blog posts	" `grep -cEi '{(presentation|poster|blog|gist)}'  *.bib` >> ./Alexiev-stats.txt
+echo "TOTAL	" `grep -cEi '@[a-z]+{'                           *.bib` >> ./Alexiev-stats.txt
