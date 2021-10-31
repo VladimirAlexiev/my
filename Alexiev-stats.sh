1adf7dd31b6fa068afb4b59907a84cca9fdcd6bc
Vladimir Alexiev <vladimir.alexiev@ontotext.com>
2021-10-31 14:52:03 +0200

    split related-bibliography.bib (other's works), correct Alexiev-stats.txt, add AlexievBoytcheva2021-SemantizatinML, IvanovAlexiev2021-EnergyKG

--- Alexiev-stats.sh
+++ Alexiev-stats.sh
@@ -3,8 +3,8 @@
 
 # ./Alexiev-stats.sh
 
-echo "Book (author), thesis, conference proceedings (editor)	" `grep -cEi '@(.*thesis|book|proceedings){'      *.bib`  > ./Alexiev-stats.txt
-echo "Tech reports, manuals, deliverables, ontologies	" `grep -cEi '@(manual|techreport){'              *.bib` >> ./Alexiev-stats.txt
-echo "Conference papers, journal articles	" `grep -cEi '@(inproceedings|article){'          *.bib` >> ./Alexiev-stats.txt
-echo "Presentations, posters, blog posts	" `grep -cEi '{(presentation|poster|blog|gist)}'  *.bib` >> ./Alexiev-stats.txt
-echo "TOTAL	" `grep -cEi '@[a-z]+{'                           *.bib` >> ./Alexiev-stats.txt
+echo "Book (author), thesis, conference proceedings (editor)	" `grep -cEi '@(.*thesis|book|proceedings){'      Alexiev-bibliography.bib`  > ./Alexiev-stats.txt
+echo "Tech reports, manuals, deliverables, ontologies	" `grep -cEi '@(manual|techreport){'              Alexiev-bibliography.bib` >> ./Alexiev-stats.txt
+echo "Conference papers, journal articles	" `grep -cEi '@(inproceedings|article){'          Alexiev-bibliography.bib` >> ./Alexiev-stats.txt
+echo "Presentations, posters, blog posts	" `grep -cEi '{(presentation|poster|blog|gist)}'  Alexiev-bibliography.bib` >> ./Alexiev-stats.txt
+echo "TOTAL	" `grep -cEi '@[a-z]+{'                           Alexiev-bibliography.bib` >> ./Alexiev-stats.txt
